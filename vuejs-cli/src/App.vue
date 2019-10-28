422002098a36945f9b19bb01ab41a48854034579
Ezequiel Brilhante <ezequielrb7@gmail.com>
2019-10-28 16:07:51 -0300

    finalização

---
+++ vuejs-cli/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Comments />
+</template>
+
+<script>
+import Comments from './components/Comments.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Comments
+  }
+}
+</script>
+
+<style>
+
+</style>
